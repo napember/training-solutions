1214f09f11e1b3578a943feea967c66b26d6bdcb
banhegyiai <banhegyi.attilaimre@nisz.hu>
2022-02-09 08:36:36 +0100

    linebreak

---
+++ src/main/java/linebreak/Car.java
@@ -0,0 +1,7 @@
+package linebreak;
+
+public class Car {
+    public String getBrandAndTypeInSeparateLines(String brand, String type) {
+    return brand + System.lineSeparator() + type;
+    }
+}
---
+++ src/main/java/linebreak/CarMain.java
@@ -0,0 +1,9 @@
+package linebreak;
+
+public class CarMain {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        System.out.print( car.getBrandAndTypeInSeparateLines("Mazda","3") );
+    }
+}
